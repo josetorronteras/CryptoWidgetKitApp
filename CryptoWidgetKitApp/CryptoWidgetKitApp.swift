484a63265bd1f1502fa256f9d34d32e7a988a39a
Jose Torronteras <7280807+josetorronteras@users.noreply.github.com>
2024-01-27 12:57:00 +0100

    chore: Project creation

---
+++ CryptoWidgetKitApp/CryptoWidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWidgetKitApp.swift
+//  CryptoWidgetKitApp
+//
+//  Created by Jose Jesus Torronteras Hernandez on 27/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
