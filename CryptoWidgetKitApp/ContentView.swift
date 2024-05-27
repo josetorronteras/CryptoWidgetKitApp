0cfffed223e1929962087c188641cd8918ba597d
Jose Torronteras <7280807+josetorronteras@users.noreply.github.com>
2024-05-27 13:50:04 +0200

    chore: Project creation (#1)

---
+++ CryptoWidgetKitApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoWidgetKitApp
+//
+//  Created by Jose Jesus Torronteras Hernandez on 27/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
