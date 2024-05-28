df619d18d80cc455cc813d56b64058a176168a9a
Jose Torronteras <7280807+josetorronteras@users.noreply.github.com>
2024-05-28 14:03:50 +0200

    feat(Crypto List): Add loading, error and refresh to view

--- CryptoWidgetKitApp/ContentView.swift
+++ CryptoWidgetKitApp/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @Environment(CryptoViewModel.self) var cryptoViewModel
     
     var body: some View {
+        @Bindable var cryptoViewModel = cryptoViewModel
         NavigationStack {
             List(cryptoViewModel.cryptos, id: \.coinInfo.id) { crypto in
                 HStack {
@@ -20,7 +21,27 @@ struct ContentView: View {
                     Text(crypto.display?.usd.price ?? "0")
                 }
             }
+            .redacted(reason: cryptoViewModel.isLoading ? .placeholder : [])
             .task { await cryptoViewModel.fetch() }
+            .refreshable { await cryptoViewModel.fetch() }
+            .alert(isPresented: $cryptoViewModel.showError, content: {
+                Alert(title: Text("An error occurred, try again later"),
+                      dismissButton: .default(
+                        Text("Retry"),
+                        action: {
+                            Task {
+                                await cryptoViewModel.fetch()
+                            }
+                        }))
+            })
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Text("API Cache 120 seconds")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                        .frame(maxWidth: .infinity)
+                }
+            }
         }
     }
 }
--- CryptoWidgetKitApp/ViewModels/CryptoViewModel.swift
+++ CryptoWidgetKitApp/ViewModels/CryptoViewModel.swift
@@ -13,13 +13,17 @@ final class CryptoViewModel {
     
     // MARK: - Properties
     private(set) var cryptos: [Crypto] = []
+    private(set) var isLoading: Bool = true
+    var showError: Bool = false
     
     // MARK: - Dependencies
     private let apiService: CryptoAPIService
     
     // MARK: - Init
-    init(apiService: CryptoAPIService = CryptoAPIService()) {
+    init(apiService: CryptoAPIService = CryptoAPIService(),
+         initialState: [Crypto] = .mock) {
         self.apiService = apiService
+        self.cryptos = initialState
     }
 }
 
@@ -28,10 +32,25 @@ extension CryptoViewModel {
     
     /// Fetch the cryptos from API and update the cryptos array
     func fetch() async {
+        isLoading = true
         do {
+            sleep(3)
             cryptos = try await apiService.fetchRetrieveFullList().data
+            isLoading = false
         } catch {
             print(error)
+            showError = true
+        }
+    }
+}
+
+// MARK: - Crypto Extension
+extension [Crypto] {
+    
+    /// Mock Crypto
+    static var mock: [Crypto] {
+        (0..<5).map {
+            Crypto(coinInfo: CoinInfo(id: $0.description, name: "name", fullName: "fullName", imageURL: "imageURL"), display: nil)
         }
     }
 }
